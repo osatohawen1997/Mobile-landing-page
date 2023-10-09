f3a600c45fab234dd4b67ca15de78b2d01ff3630
osatohawen1997 <idahosavictor925@gmail.com>
2023-10-09 06:13:07 -0700

    Initial commit

---
+++ javascript/main.js
@@ -0,0 +1,5 @@
+// var head = document.querySelector('.scroll');
+// window.addEventListener('scroll',() =>{
+//     var change = document.querySelector('scroll')
+//     change.classList.add('.active',window.scrollY > 0);
+// });
